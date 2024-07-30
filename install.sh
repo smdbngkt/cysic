efffc6f71e67dff0cc6a9e822cc639391cdea304
Dimas Bangkit Satriawan <57074351+smdbngkt@users.noreply.github.com>
2024-07-30 14:47:59 +0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -21,7 +21,7 @@ echo -e "${RED}(_______/  |___|\\__/|___|(________/  ${NC}"
 echo -e "${CYAN} Cysic Testnet Node Installation ${NC}"
 
 # Remove old directory and create new one
-rm -rf ~/.cysic
+
 rm -rf ~/cysic-verifier
 mkdir ~/cysic-verifier
 
